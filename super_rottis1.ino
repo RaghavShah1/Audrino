9b224a02e5cec4337227cd09400a3021a5804cee
RaghavShah1 <97252904+RaghavShah1@users.noreply.github.com>
2022-01-07 15:31:38 +0530

    Add files via upload

---
+++ super_rottis1.ino
@@ -0,0 +1,39 @@
+// C++ code
+//
+int btnstate = 0;
+
+void setup()
+{
+  pinMode(5, INPUT);
+  pinMode(13, OUTPUT);
+  pinMode(4, INPUT);
+  pinMode(12, OUTPUT);
+  pinMode(3, INPUT);
+  pinMode(11, OUTPUT);
+}
+
+void loop()
+{
+  if (digitalRead(5) == 1) {
+    digitalWrite(13, HIGH);
+  } else {
+    digitalWrite(13, LOW);
+  }
+  while (digitalRead(4) == 1) {
+    digitalWrite(12, HIGH);
+    delay(300); // Wait for 300 millisecond(s)
+    digitalWrite(12, LOW);
+    delay(300); // Wait for 300 millisecond(s)
+  }
+  if (digitalRead(3) == 1) {
+    if (btnstate == 1) {
+      digitalWrite(11, LOW);
+      btnstate = 0;
+      delay(600); // Wait for 600 millisecond(s)
+    } else {
+      btnstate = 1;
+      digitalWrite(11, HIGH);
+      delay(600); // Wait for 600 millisecond(s)
+    }
+  }
+}
